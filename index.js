13fdfca22f749b66fff809a33d9e6accbf968fb3
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-04-09 09:58:23 -0400

    safghjk
    sdfghbjnm,

--- index.js
+++ index.js
@@ -74,6 +74,43 @@ const titles = [
   "Little Women",
 ];
 
+
+var questions = [
+  {
+      question: "What is 36 + 42",
+      a: "64",
+      b: "78",
+      c: "76",
+      answer: "B"
+    },
+  {
+      question: "What is 7 x 4?",
+      a: "21",
+      b: "27",
+      c: "28",
+      answer: "C"
+    },
+  {
+      question: "What is 16 / 4?",
+      a: "4",
+      b: "6",
+      c: "3",
+      answer: "A"
+    },
+  {
+      question: "What is 8 x 12?",
+      a: "88",
+      b: "112",
+      c: "96",
+      answer: "C"
+    }
+  ];
+
+
+
+
+
+
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 
