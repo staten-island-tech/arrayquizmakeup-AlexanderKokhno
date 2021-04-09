af8f05266be6b3336191b7973658a89c49e66f36
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-04-09 10:00:08 -0400

    rt56

--- index.js
+++ index.js
@@ -75,36 +75,7 @@ const titles = [
 ];
 
 
-var questions = [
-  {
-      question: "What is 36 + 42",
-      a: "64",
-      b: "78",
-      c: "76",
-      answer: "B"
-    },
-  {
-      question: "What is 7 x 4?",
-      a: "21",
-      b: "27",
-      c: "28",
-      answer: "C"
-    },
-  {
-      question: "What is 16 / 4?",
-      a: "4",
-      b: "6",
-      c: "3",
-      answer: "A"
-    },
-  {
-      question: "What is 8 x 12?",
-      a: "88",
-      b: "112",
-      c: "96",
-      answer: "C"
-    }
-  ];
+
 
 
 
